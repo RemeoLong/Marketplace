a67e9ecd9fbe93949e3e92296f5f48b3614b0eb1
Long Tran <RemeoLongQuangTran@gmail.com>
2024-10-17 22:27:54 -0500

    Setting up MSSQL connection

--- Market/settings.py
+++ Market/settings.py
@@ -76,13 +76,6 @@ WSGI_APPLICATION = 'Market.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-'''DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
 # settings.py
 DATABASES = {
     "default": {
