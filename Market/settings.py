8df2167e31b723b7c57c83fead548539d7307658
Long Tran <RemeoLongQuangTran@gmail.com>
2024-10-18 19:55:17 -0500

    Deployed Marketplace on Pythonanywhere.com URL: remlongmarketplace.pythonanywhere.com

--- Market/settings.py
+++ Market/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'remlongmarketplace.pythonanywhere.com']
 
 
 # Application definition
